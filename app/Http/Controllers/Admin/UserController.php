118ba6eaebfde29f4a48cad69deb942d55eb06e5
Huy Ho <b1.maitrongtoi@gmail.com>
2019-07-17 09:15:56 +0700

    up

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -69,7 +69,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id, Request $request)
     {
         //
         $user = User::find($id);
